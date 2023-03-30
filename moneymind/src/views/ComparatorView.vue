cd9e935262262733bd7e25acf599c8b05baa96bc
Higor Mariano <72944799+Higor-SM@users.noreply.github.com>
2023-03-30 20:20:48 -0300

    feat: creating Buttons and Dropdowns

--- moneymind/src/views/ComparatorView.vue
+++ moneymind/src/views/ComparatorView.vue
@@ -1,7 +1,16 @@
 <template>
-  <div>
-    <h1>This is a comparator page</h1>
-  </div>
+  <UsualDropdown title="Services"/>
 </template>
+
+<script lang="ts">
+import UsualDropdown from '../components/UsualDropdown.vue';
+
+export default {
+  name: 'comparator',
+  components: {
+    UsualDropdown
+  }
+}
+</script>
 
 <style></style>
