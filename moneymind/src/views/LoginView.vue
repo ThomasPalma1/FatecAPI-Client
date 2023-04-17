687de869bcb0b58d88818e8d008c528478c8e499
Barbara <60778277+Barbara-BB@users.noreply.github.com>
2023-04-17 09:59:35 -0300

    Create initial view Login and router login

---
+++ moneymind/src/views/LoginView.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="body">
+    <h1 class="title">MoneyMind - Login</h1>
+    <p class="title">
+      O seu login é opcional, mas ativa sugestões especiais para você
+    </p>
+    <div>
+      <FilterSearch @data-change="dataOnChange" />
+    </div>
+    <div class="container" v-if="!isEmpty(data)">
+      <ChartVue
+        :chart-type="chartType"
+        :chart-title="chartTitle"
+        :data="data"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.body {
+  padding: 1.25rem;
+}
+.title {
+  text-align: center;
+}
+</style>
+
+<script lang="ts">
+import FilterSearch from "@/components/FilterSearch.vue";
+import ChartVue from "@/components/charts/Chart.vue";
+
+export default {
+  components: {
+    FilterSearch,
+    ChartVue,
+  },
+  computed: {
+    isEmpty() {
+      return (arr: never[]) => arr.length === 0;
+    },
+  },
+  data() {
+    return {
+      data: [],
+      chartType: "",
+      chartTitle: "",
+    };
+  },
+  methods: {
+    dataOnChange(data: [], type: string, title: string) {
+      this.data = data;
+      this.chartType = type;
+      this.chartTitle = title;
+    },
+  },
+};
+</script>
